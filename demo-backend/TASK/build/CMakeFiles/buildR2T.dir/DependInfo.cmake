2f21497b4e93437b69137f0baab68228a3b9b04e
dlhu <88300940+dlhu@users.noreply.github.com>
2023-03-22 23:20:19 +0800

    Add files via upload

---
+++ demo-backend/TASK/build/CMakeFiles/buildR2T.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jin/demo/TASK/buildR2T.cpp" "CMakeFiles/buildR2T.dir/buildR2T.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/buildR2T.cpp.o.d"
+  "/home/jin/demo/TASK/util/PB.cpp" "CMakeFiles/buildR2T.dir/util/PB.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/util/PB.cpp.o.d"
+  "/home/jin/demo/TASK/util/canLabel.cpp" "CMakeFiles/buildR2T.dir/util/canLabel.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/util/canLabel.cpp.o.d"
+  "/home/jin/demo/TASK/util/canLabelET.cpp" "CMakeFiles/buildR2T.dir/util/canLabelET.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/util/canLabelET.cpp.o.d"
+  "/home/jin/demo/TASK/util/ioPB.cpp" "CMakeFiles/buildR2T.dir/util/ioPB.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/util/ioPB.cpp.o.d"
+  "/home/jin/demo/TASK/util/ioUint.cpp" "CMakeFiles/buildR2T.dir/util/ioUint.cpp.o" "gcc" "CMakeFiles/buildR2T.dir/util/ioUint.cpp.o.d"
+  "/home/jin/demo/TASK/util/mtrie.cc" "CMakeFiles/buildR2T.dir/util/mtrie.cc.o" "gcc" "CMakeFiles/buildR2T.dir/util/mtrie.cc.o.d"
+  "/home/jin/demo/TASK/util/trie.cc" "CMakeFiles/buildR2T.dir/util/trie.cc.o" "gcc" "CMakeFiles/buildR2T.dir/util/trie.cc.o.d"
+  "/home/jin/demo/TASK/util/util.cc" "CMakeFiles/buildR2T.dir/util/util.cc.o" "gcc" "CMakeFiles/buildR2T.dir/util/util.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
