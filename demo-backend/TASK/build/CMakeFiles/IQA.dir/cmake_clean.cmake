7d8f089a534057cd0ce744f554aca907ed6e58a3
dlhu <88300940+dlhu@users.noreply.github.com>
2023-03-22 23:23:28 +0800

    Add files via upload

---
+++ demo-backend/TASK/build/CMakeFiles/IQA.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IQA.dir/IQA.cpp.o"
+  "CMakeFiles/IQA.dir/IQA.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/PB.cpp.o"
+  "CMakeFiles/IQA.dir/util/PB.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/canLabel.cpp.o"
+  "CMakeFiles/IQA.dir/util/canLabel.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/canLabelET.cpp.o"
+  "CMakeFiles/IQA.dir/util/canLabelET.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/ioPB.cpp.o"
+  "CMakeFiles/IQA.dir/util/ioPB.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/ioUint.cpp.o"
+  "CMakeFiles/IQA.dir/util/ioUint.cpp.o.d"
+  "CMakeFiles/IQA.dir/util/mtrie.cc.o"
+  "CMakeFiles/IQA.dir/util/mtrie.cc.o.d"
+  "CMakeFiles/IQA.dir/util/trie.cc.o"
+  "CMakeFiles/IQA.dir/util/trie.cc.o.d"
+  "CMakeFiles/IQA.dir/util/util.cc.o"
+  "CMakeFiles/IQA.dir/util/util.cc.o.d"
+  "IQA"
+  "IQA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IQA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
